b30e7bd1fe952335d68dcfbbca920820ccf5d514
Luis Eraso <Luis Eraso@marte>
2017-04-18 19:39:37 -0500

    Agregar proyecto ejb+jpa

--- jee-03-ejbs_y_jpa/src/main/java/co/edu/ims/ejbs/PersonaEJB.java
+++ jee-03-ejbs_y_jpa/src/main/java/co/edu/ims/ejbs/PersonaEJB.java
@@ -4,16 +4,30 @@ import co.edu.ims.modelo.Persona;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 @Stateless
 @Path("/persona")
 public class PersonaEJB {
     
-    @PersistenceContext(unitName = "personaPU")
+    @PersistenceContext(unitName = "peliculasPU")
     protected EntityManager em;
     
-    
+    @GET
+    @Path("{id}")
+    @Produces("application/json")       
+    public Persona buscar(@PathParam("id") Integer pId){
+        return em.find(Persona.class, pId);        
+    }
+
+    @PUT
+    @Consumes("application/json")
+    @Produces("application/json")
     public Persona agregar(Persona entity){
         em.persist(entity);
         em.flush();
